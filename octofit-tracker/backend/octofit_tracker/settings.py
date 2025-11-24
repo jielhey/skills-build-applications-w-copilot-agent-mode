1ab6cc18eb1ce69da151b16776dad4125bd469c2
jielhey <jielhey.ann.musni@accenture.com>
2025-11-24 20:58:24 +0000

    Update API endpoint URLs and ALLOWED_HOSTS for Codespace compatibility

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-%fd9$9eyp%-xuozaa%@xu$+9j8!2y2!13f_*dn&0e&-(i8c)%x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
